b29995a392619ad01551396b6572def20a0bb008
Mohamed Alsharaf <mohamed.alsharaf@silverstripe.com>
2022-01-26 21:53:08 +1300

    Update codebase standard to comply with phpcs

--- tests/SiteConfigExtensionTest.php
+++ tests/SiteConfigExtensionTest.php
@@ -2,22 +2,26 @@
 
 namespace NZTA\SiteBanner\Tests;
 
-use NZTA\SiteBanner\Models\SiteBanner;
 use NZTA\SiteBanner\Extensions\SiteConfigExtension;
+use NZTA\SiteBanner\Models\SiteBanner;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\SiteConfig\SiteConfig;
 
+/**
+ * @phpcs:disable SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingAnyTypeHint
+ * @phpcs:disable SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingAnyTypeHint
+ */
 class SiteConfigExtensionTest extends SapphireTest
 {
     protected $usesDatabase = true;
 
     protected static $required_extensions = [
         SiteConfig::class => [
-            SiteConfigExtension::class
-        ]
+            SiteConfigExtension::class,
+        ],
     ];
 
-    public function testFiltersInactiveBanners()
+    public function testFiltersInactiveBanners(): void
     {
         $activeBanner = new SiteBanner();
         $activeBanner->Content = 'test';
